305b646e929b4107ca676da1e08b69598f165fe4
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-07-18 13:48:54 +0800

    JEP 359: Records (Preview)

---
+++ jdk17-test/src/com/czhao/test/jdk14/TestRecord.java
@@ -0,0 +1,35 @@
+package com.czhao.test.jdk14;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * JEP 359: Records (Preview)
+ *
+ * @author zhaochun
+ */
+public class TestRecord {
+    public static void main(String[] args) {
+        Point p1 = new Point(10, 20);
+        Point p2 = new Point(10, 20);
+        System.out.println(p1.x());
+        System.out.println(p1.y());
+        System.out.println(p1.hashCode());
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.toString());
+
+        List<Point> points = Arrays.stream(new Point[]{p1, p2}).collect(Collectors.toList());
+        PointGrp pointGrp = new PointGrp(points, "pg");
+        System.out.println(pointGrp.toString());
+
+        pointGrp.points().add(new Point(0, 0));
+        System.out.println(pointGrp.toString());
+    }
+
+    record Point(int x, int y) {
+    }
+
+    record PointGrp(List<Point> points, String grpName) {
+    }
+}

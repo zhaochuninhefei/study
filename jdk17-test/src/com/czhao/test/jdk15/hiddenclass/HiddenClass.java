e30fbf1c7287724c0df15d649bc662fbd83b69b8
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-07-19 18:59:05 +0800

    JEP 371: Hidden Classes

---
+++ jdk17-test/src/com/czhao/test/jdk15/hiddenclass/HiddenClass.java
@@ -0,0 +1,12 @@
+package com.czhao.test.jdk15.hiddenclass;
+
+/**
+ * JEP 371: Hidden Classes
+ *
+ * @author zhaochun
+ */
+public class HiddenClass {
+    public String sayHello(String name) {
+        return "Hello, " + name;
+    }
+}

7996acb192d8af03670f9b950aef4044b10889b3
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-07-25 10:09:40 +0800

    JEP 415:Context-Specific Deserialization Filters

--- jdk17-test/src/com/czhao/test/jdk17/TestContextSpecificDeserializationFilters.java
+++ jdk17-test/src/com/czhao/test/jdk17/TestContextSpecificDeserializationFilters.java
@@ -10,12 +10,18 @@ import java.io.*;
 public class TestContextSpecificDeserializationFilters {
     public static void main(String[] args) {
         TestContextSpecificDeserializationFilters me = new TestContextSpecificDeserializationFilters();
+        // 创建对象 person
         Person person = new Person("张三", 20, new PersonID("xxx001"));
+        // 序列化为字节流
         byte[] bytes = me.writePerson(person);
+        // 没有过滤器的反序列化
         me.readPersonWithoutFilter(bytes);
+        // 带有过滤器的反序列化，两个内部类Person与PersonID都允许反序列化
         me.readPersonWithFilter(bytes,
                 "com.czhao.test.jdk17.TestContextSpecificDeserializationFilters$Person",
                 "com.czhao.test.jdk17.TestContextSpecificDeserializationFilters$PersonID");
+        // 带有过滤器的反序列化，只有一个内部类Person允许反序列化
+        // 会抛出异常: java.io.InvalidClassException: filter status: REJECTED
         me.readPersonWithFilter(bytes,
                 "com.czhao.test.jdk17.TestContextSpecificDeserializationFilters$Person");
     }
@@ -43,6 +49,7 @@ public class TestContextSpecificDeserializationFilters {
     private void readPersonWithFilter(byte[] bytes, String... classPtns) {
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
         try (ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
+            // 编写过滤条件: 允许 classPtns 指定的所有类，允许`java.base`模块下的所有类，禁止其他类。
             String ptn = String.join(";", classPtns) + ";java.base/*;!*";
             ObjectInputFilter filter = ObjectInputFilter.Config.createFilter(ptn);
             objectInputStream.setObjectInputFilter(filter);

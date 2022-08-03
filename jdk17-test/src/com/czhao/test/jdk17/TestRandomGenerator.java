46a288af6ee3432b88c9d7d2183eea1477050dd5
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-08-03 08:39:51 +0800

    JEP 356: Enhanced Pseudo-Random Number Generators

--- jdk17-test/src/com/czhao/test/jdk17/TestRandomGenerator.java
+++ jdk17-test/src/com/czhao/test/jdk17/TestRandomGenerator.java
@@ -1,11 +1,19 @@
 package com.czhao.test.jdk17;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Comparator;
+import java.util.SplittableRandom;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import java.util.random.RandomGenerator;
 import java.util.random.RandomGeneratorFactory;
 
 /**
+ * JEP 356:	Enhanced Pseudo-Random Number Generators
+ *
  * @author zhaochun
  */
 public class TestRandomGenerator {
@@ -26,60 +34,90 @@ public class TestRandomGenerator {
 
         // Random,伪随机数生成器(线性同余算法)
         RandomGenerator random = RandomGeneratorFactory.of("Random").create();
-        System.out.println(random.nextInt());
+        System.out.println("Random : " + random.nextInt());
+        System.out.println("Random : " + random.nextInt());
 
         // SecureRandom 密码学强随机数生成器(RNG)
         RandomGenerator secureRandom = RandomGeneratorFactory.of("SecureRandom").create();
-        System.out.println(secureRandom.nextInt());
+        System.out.println("SecureRandom : " + secureRandom.nextInt());
+        System.out.println("SecureRandom : " + secureRandom.nextInt());
 
         // SplittableRandom 伪随机数生成器(SplitMix算法)
-        RandomGenerator splittableRandom = RandomGeneratorFactory.of("SplittableRandom").create();
-        System.out.println(splittableRandom.nextInt());
+        SplittableRandom splittableRandom = (SplittableRandom)RandomGeneratorFactory.of("SplittableRandom").create();
+        System.out.println("SplittableRandom in main thread : " + splittableRandom.nextInt());
+        System.out.println("SplittableRandom in main thread : " + splittableRandom.nextInt());
         // ThreadLocalRandom,伪随机数生成器(SplitMix算法)
-        System.out.println(ThreadLocalRandom.current().nextInt());
+        System.out.println("ThreadLocalRandom in main thread : " + ThreadLocalRandom.current().nextInt());
+        System.out.println("ThreadLocalRandom in main thread : " + ThreadLocalRandom.current().nextInt());
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(() -> {
+                System.out.println("ThreadLocalRandom in sub thread : " + ThreadLocalRandom.current().nextInt());
+                System.out.println("ThreadLocalRandom in sub thread : " + ThreadLocalRandom.current().nextInt());
+                System.out.println("SplittableRandom in sub thread : " + splittableRandom.split().nextInt());
+                System.out.println("SplittableRandom in sub thread : " + splittableRandom.split().nextInt());
+            });
+        }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // Xoroshiro128PlusPlus 伪随机数生成器(xoshiro/xoroshiro算法之一)
         RandomGenerator xoroshiro128PlusPlus = RandomGeneratorFactory.of("Xoroshiro128PlusPlus").create();
-        System.out.println(xoroshiro128PlusPlus.nextInt());
+        System.out.println("Xoroshiro128PlusPlus : " + xoroshiro128PlusPlus.nextInt());
+        System.out.println("Xoroshiro128PlusPlus : " + xoroshiro128PlusPlus.nextInt());
 
         // Xoshiro256PlusPlus 伪随机数生成器(xoshiro/xoroshiro算法之一)
         RandomGenerator xoshiro256PlusPlus = RandomGeneratorFactory.of("Xoshiro256PlusPlus").create();
-        System.out.println(xoshiro256PlusPlus.nextInt());
+        System.out.println("Xoshiro256PlusPlus : " + xoshiro256PlusPlus.nextInt());
+        System.out.println("Xoshiro256PlusPlus : " + xoshiro256PlusPlus.nextInt());
 
         // L128X256MixRandom 伪随机数生成器(LXM PRNG算法家族之一)
         RandomGenerator l128X256MixRandom = RandomGeneratorFactory.of("L128X256MixRandom").create();
-        System.out.println(l128X256MixRandom.nextInt());
+        System.out.println("L128X256MixRandom : " + l128X256MixRandom.nextInt());
+        System.out.println("L128X256MixRandom : " + l128X256MixRandom.nextInt());
 
         // L128X128MixRandom 伪随机数生成器(LXM PRNG算法家族之一)
         RandomGenerator l128X128MixRandom = RandomGeneratorFactory.of("L128X128MixRandom").create();
-        System.out.println(l128X128MixRandom.nextInt());
+        System.out.println("L128X128MixRandom : " + l128X128MixRandom.nextInt());
+        System.out.println("L128X128MixRandom : " + l128X128MixRandom.nextInt());
 
         // L128X1024MixRandom 伪随机数生成器(LXM PRNG算法家族之一)
         RandomGenerator l128X1024MixRandom = RandomGeneratorFactory.of("L128X1024MixRandom").create();
-        System.out.println(l128X1024MixRandom.nextInt());
+        System.out.println("L128X1024MixRandom : " + l128X1024MixRandom.nextInt());
+        System.out.println("L128X1024MixRandom : " + l128X1024MixRandom.nextInt());
 
         // L64X256MixRandom 伪随机数生成器(LXM PRNG算法家族之一)
         RandomGenerator l64X256MixRandom = RandomGeneratorFactory.of("L64X256MixRandom").create();
-        System.out.println(l64X256MixRandom.nextInt());
+        System.out.println("L64X256MixRandom : " + l64X256MixRandom.nextInt());
+        System.out.println("L64X256MixRandom : " + l64X256MixRandom.nextInt());
 
         // L64X128StarStarRandom 伪随机数生成器(LXM PRNG算法家族之一)
         RandomGenerator l64X128StarStarRandom = RandomGeneratorFactory.of("L64X128StarStarRandom").create();
-        System.out.println(l64X128StarStarRandom.nextInt());
+        System.out.println("L64X128StarStarRandom : " + l64X128StarStarRandom.nextInt());
+        System.out.println("L64X128StarStarRandom : " + l64X128StarStarRandom.nextInt());
 
         // L64X128MixRandom 伪随机数生成器(LXM PRNG算法家族之一)
         RandomGenerator l64X128MixRandom = RandomGeneratorFactory.of("L64X128MixRandom").create();
-        System.out.println(l64X128MixRandom.nextInt());
+        System.out.println("L64X128MixRandom : " + l64X128MixRandom.nextInt());
+        System.out.println("L64X128MixRandom : " + l64X128MixRandom.nextInt());
 
         // L64X1024MixRandom 伪随机数生成器(LXM PRNG算法家族之一)
         RandomGenerator l64X1024MixRandom = RandomGeneratorFactory.of("L64X1024MixRandom").create();
-        System.out.println(l64X1024MixRandom.nextInt());
+        System.out.println("L64X1024MixRandom : " + l64X1024MixRandom.nextInt());
+        System.out.println("L64X1024MixRandom : " + l64X1024MixRandom.nextInt());
 
         // L32X64MixRandom 伪随机数生成器(LXM PRNG算法家族之一)
-        RandomGenerator l32X64MixRandom = RandomGeneratorFactory.of("L32X64MixRandom").create();
-        System.out.println(l32X64MixRandom.nextInt());
+        RandomGenerator l32X64MixRandom = RandomGeneratorFactory.of("L32X64MixRandom").create(LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli());
+        System.out.println("L32X64MixRandom : " + l32X64MixRandom.nextInt());
+        System.out.println("L32X64MixRandom : " + l32X64MixRandom.nextInt());
 
         // JDK17默认随机数生成器 L32X64MixRandom
         RandomGenerator generator = RandomGenerator.getDefault();
-        System.out.println(generator.nextInt());
+        System.out.println("RandomGenerator Default : " + generator.nextInt());
+        System.out.println("RandomGenerator Default : " + generator.nextInt());
     }
 }

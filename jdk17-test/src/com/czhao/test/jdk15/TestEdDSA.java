6e77bffcbf4efb2fdb21569b475d8951359295ee
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-08-10 08:37:27 +0800

    JEP 339: Edwards-Curve Digital Signature Algorithm (EdDSA)

--- jdk17-test/src/com/czhao/test/jdk15/TestEdDSA.java
+++ jdk17-test/src/com/czhao/test/jdk15/TestEdDSA.java
@@ -17,7 +17,10 @@ public class TestEdDSA {
         me.testECDSA("测试 ECDSA 签名");
         me.testECDSA("");
     }
-
+    /**
+     * EdDSA是`Edwards-curve Digital Signature Algorithm`的缩写，意思是爱德华曲线数字签名算法。
+     * 爱德华曲线是一种椭圆曲线，比如`Curve25519`。
+     */
     private void testEdDSA(String msg) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
         // 获取明文字节数组
         byte[] plaintext = msg.getBytes(StandardCharsets.UTF_8);
@@ -47,6 +50,9 @@ public class TestEdDSA {
         System.out.println(signVerify);
     }
 
+    /**
+     * ECDSA是`Elliptic Curve Digital Signature Algorithm`的缩写，意思是椭圆曲线数字签名算法。
+     */
     private void testECDSA(String msg) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
         // 明文
         byte[] plaintext = msg.getBytes(StandardCharsets.UTF_8);

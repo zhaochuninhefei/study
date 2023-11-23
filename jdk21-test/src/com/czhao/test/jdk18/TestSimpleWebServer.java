435570b081dc1b1d97c9f95ea8f0140ceefa4e03
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-11-23 21:58:22 +0800

    提交代码

--- jdk21-test/src/com/czhao/test/jdk18/TestSimpleWebServer.java
+++ jdk21-test/src/com/czhao/test/jdk18/TestSimpleWebServer.java
@@ -1,7 +1,8 @@
 package com.czhao.test.jdk18;
 
-import com.sun.net.httpserver.SimpleFileServer;
+import com.sun.net.httpserver.*;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.file.Path;
 
@@ -23,23 +24,29 @@ public class TestSimpleWebServer {
         $ jwebserver -p 9000
 
         访问URL: http://localhost:9000/
+
+        默认情况下，jwebserver的文件根目录是执行命令的当前目录，可以通过`-d`选择指定目录。
+
+        仅支持幂等的 `HEAD` 和 `GET` 请求。
      */
 
 
     /**
      * API 方式启动简易网络服务器
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TestSimpleWebServer me = new TestSimpleWebServer();
-        me.startSimpleServer();
+        me.startSimpleFileServer();
     }
 
-    private void startSimpleServer() {
+    private void startSimpleFileServer() {
         var server = SimpleFileServer.createFileServer(new InetSocketAddress(9000),
-                Path.of("/home/zhaochun/work/sources/github.com/zhaochuninhefei/study/jdk21-test/resource/simpleWeb/files"),
+                Path.of("/home/zhaochun/work/sources/github.com/zhaochuninhefei/study/jdk21-test/resource/simpleWeb"),
                 SimpleFileServer.OutputLevel.VERBOSE);
         server.start();
 
-        // 启动后可访问: http://localhost:9000/index.html
+        // 启动后可访问: http://localhost:9000
     }
+
+
 }

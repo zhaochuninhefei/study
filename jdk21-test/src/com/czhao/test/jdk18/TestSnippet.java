3ed2924d27a2657ffc4aa7b426c33145edb9d3de
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-11-24 23:35:31 +0800

    提交代码

---
+++ jdk21-test/src/com/czhao/test/jdk18/TestSnippet.java
@@ -0,0 +1,41 @@
+package com.czhao.test.jdk18;
+
+/**
+ * @author zhaochun
+ *
+ */
+public class TestSnippet {
+
+    /**
+     * testCodeInPre
+     *
+     * <P>Use Sample:</P>
+     * <pre>{@code
+     *     TestSnippet testSnippet = new TestSnippet();
+     *     // call testCodeInPre
+     *     testSnippet.testCodeInPre();
+     *     List<String> list = new ArrayList<>();
+     * }</pre>
+     *
+     */
+    public void testCodeInPre() {
+        System.out.println("testCodeInPre...");
+    }
+
+    /**
+     * testSnippet
+     *
+     * <P>Use Sample:</P>
+     * {@snippet :
+     *     TestSnippet testSnippet = new TestSnippet();
+     *     // call testSnippet
+     *     testSnippet.testSnippet();
+     *     List<String> list = new ArrayList<>();
+     * }
+     *
+     */
+    public void testSnippet() {
+        System.out.println("testSnippet...");
+    }
+
+}

5e61cdbd44d5930fcaf954e76e15cce9fa6ca313
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-04 19:52:28 +0800

    提交代码

---
+++ jdk21-test/src/com/czhao/test/jdk18/TestInetAddress.java
@@ -0,0 +1,10 @@
+package com.czhao.test.jdk18;
+
+/**
+ * @author zhaochun
+ */
+public class TestInetAddress {
+    public static void main(String[] args) {
+        TestInetAddress me = new TestInetAddress();
+    }
+}

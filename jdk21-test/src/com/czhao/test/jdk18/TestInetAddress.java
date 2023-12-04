28b87465db9643bb89b95d869643d377c428871e
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-04 19:55:17 +0800

    提交代码

--- jdk21-test/src/com/czhao/test/jdk18/TestInetAddress.java
+++ jdk21-test/src/com/czhao/test/jdk18/TestInetAddress.java
@@ -1,10 +1,26 @@
 package com.czhao.test.jdk18;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * @author zhaochun
  */
 public class TestInetAddress {
     public static void main(String[] args) {
         TestInetAddress me = new TestInetAddress();
+        me.test01();
+    }
+
+    private void test01() {
+        var addressBytes = new byte[] { 127, 0, 0, 1 };
+        String resolveHostName;
+        try {
+            resolveHostName = InetAddress.getByAddress(addressBytes)
+                    .getCanonicalHostName();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(resolveHostName);
     }
 }

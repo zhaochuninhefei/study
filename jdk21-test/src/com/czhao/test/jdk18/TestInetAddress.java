d343ecafc3e932f921fa10ceb21dc71507c9d384
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-04 19:57:58 +0800

    提交代码

--- jdk21-test/src/com/czhao/test/jdk18/TestInetAddress.java
+++ jdk21-test/src/com/czhao/test/jdk18/TestInetAddress.java
@@ -2,6 +2,7 @@ package com.czhao.test.jdk18;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 /**
  * @author zhaochun
@@ -13,14 +14,29 @@ public class TestInetAddress {
     }
 
     private void test01() {
+        // 根据 IP 查找 hostname
         var addressBytes = new byte[] { 127, 0, 0, 1 };
         String resolveHostName;
         try {
             resolveHostName = InetAddress.getByAddress(addressBytes)
                     .getCanonicalHostName();
+            System.out.println(resolveHostName);
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(resolveHostName);
+
+        // 根据 hostname 查找 IP
+        String hostname = "localhost";
+        try {
+            InetAddress address = InetAddress.getByName(hostname);
+            System.out.println(Arrays.toString(address.getAddress()));
+
+            InetAddress[] addresses = InetAddress.getAllByName(hostname);
+            for (InetAddress ads : addresses) {
+                System.out.println(Arrays.toString(ads.getAddress()));
+            }
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

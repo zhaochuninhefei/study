1b46134550cfe3e135c239918ca665ec8beab93e
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-20 18:39:35 +0800

    提交代码

--- jdk21-test/src/com/czhao/test/jdk21/TestStringTemplate.java
+++ jdk21-test/src/com/czhao/test/jdk21/TestStringTemplate.java
@@ -1,7 +1,20 @@
 package com.czhao.test.jdk21;
 
+import static java.lang.StringTemplate.STR;
+
 /**
  * @author zhaochun
  */
 public class TestStringTemplate {
+    public static void main(String[] args) {
+        TestStringTemplate me = new TestStringTemplate();
+        me.test01();
+    }
+
+    private void test01() {
+        var name = "zhaochun";
+        // 要使用 StringTemplate, 请将工程的java编译level调整为 21 的 Preview 预览版本
+        var message = STR."Welcome to use String Template, \{name} !";
+        System.out.println(message);
+    }
 }

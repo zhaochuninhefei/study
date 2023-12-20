a15db46b7883cfda0530d0368798b3f49c6d2f8a
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-20 19:08:11 +0800

    提交代码

--- jdk21-test/src/com/czhao/test/jdk21/TestStringTemplate.java
+++ jdk21-test/src/com/czhao/test/jdk21/TestStringTemplate.java
@@ -1,5 +1,7 @@
 package com.czhao.test.jdk21;
 
+import java.io.File;
+
 import static java.lang.StringTemplate.STR;
 
 /**
@@ -14,29 +16,35 @@ public class TestStringTemplate {
     private void test01() {
         var name = "zhaochun";
         // 要使用 StringTemplate, 请将工程的java编译level调整为 21 的 Preview 预览版本
-        var message = STR."Welcome to use String Template, \{name} !";
+        var message = STR. "Welcome to use String Template, \{ name } !" ;
         System.out.println(message);
 
         // 直接埋入字符串
         String firstName = "Chun";
-        String lastName  = "Zhao";
-        String fullName  = STR."\{firstName} \{lastName}";
+        String lastName = "Zhao";
+        String fullName = STR. "\{ firstName } \{ lastName }" ;
         System.out.println(fullName);
 
-        String sortName  = STR."\{lastName}, \{firstName}";
+        String sortName = STR. "\{ lastName }, \{ firstName }" ;
         System.out.println(sortName);
 
         // 埋入表达式,运行时会执行表达式进行计算 如这里的 x + y
         int x = 10, y = 20;
-        String s = STR."\{x} + \{y} = \{x + y}";
+        String s = STR. "\{ x } + \{ y } = \{ x + y }" ;
         System.out.println(s);
 
         // 埋入方法，可访问的字段
         Poetry p = new Poetry("李白", "少年行");
-        String line1 = STR."少年负\{getSomething()}，奋烈自有时。";
-        String line2 = STR."作者: \{p.author} 《\{p.title}》";
+        String line1 = STR. "少年负\{ getSomething() }，奋烈自有时。" ;
+        String line2 = STR. "作者: \{ p.author } 《\{ p.title }》" ;
         System.out.println(line1);
         System.out.println(line2);
+
+        // 直接在内部使用双引号
+        String filePath = "tmp.dat";
+        File file = new File(filePath);
+        String msg = STR. "The file \{ filePath } \{ file.exists() ? "does" : "does not" } exist" ;
+        System.out.println(msg);
     }
 
     private String getSomething() {

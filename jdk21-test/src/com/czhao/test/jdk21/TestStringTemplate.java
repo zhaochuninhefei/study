acb7728de250e4e737605fced0ecfbb3ac8b7a58
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-20 18:43:59 +0800

    提交代码

--- jdk21-test/src/com/czhao/test/jdk21/TestStringTemplate.java
+++ jdk21-test/src/com/czhao/test/jdk21/TestStringTemplate.java
@@ -17,7 +17,7 @@ public class TestStringTemplate {
         var message = STR."Welcome to use String Template, \{name} !";
         System.out.println(message);
 
-        // Embedded expressions can be strings
+        // 直接埋入字符串
         String firstName = "Chun";
         String lastName  = "Zhao";
         String fullName  = STR."\{firstName} \{lastName}";
@@ -26,11 +26,12 @@ public class TestStringTemplate {
         String sortName  = STR."\{lastName}, \{firstName}";
         System.out.println(sortName);
 
-//        // Embedded expressions can perform arithmetic
-//        int x = 10, y = 20;
-//        String s = STR."\{x} + \{y} = \{x + y}";
-//
-//
+        // 埋入表达式,运行时会执行表达式进行计算 如这里的 x + y
+        int x = 10, y = 20;
+        String s = STR."\{x} + \{y} = \{x + y}";
+        System.out.println(s);
+
+
 //        // Embedded expressions can invoke methods and access fields
 //        String s = STR."You have a \{getOfferType()} waiting for you!";
 //

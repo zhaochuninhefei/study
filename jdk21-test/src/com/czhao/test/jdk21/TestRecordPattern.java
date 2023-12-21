d42c228790a04eadf2646e0f57d4c60a005fae87
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-21 15:18:56 +0800

    提交代码

--- jdk21-test/src/com/czhao/test/jdk21/TestRecordPattern.java
+++ jdk21-test/src/com/czhao/test/jdk21/TestRecordPattern.java
@@ -12,11 +12,31 @@ public class TestRecordPattern {
     private void test01() {
         Point p = new Point(1, 2);
         printSum(p);
+
+        Rectangle r = new Rectangle(
+                new ColoredPoint(new Point(1, 2), Color.RED),
+                new ColoredPoint(new Point(3, 4), Color.BLUE)
+        );
+        printUpperLeftColoredPointNoNesting(r);
+        printColorOfUpperLeftPointNesting(r);
     }
 
     static void printSum(Object obj) {
         if (obj instanceof Point(int x, int y)) {
             System.out.println(x + y);
+        }
+    }
+
+    static void printUpperLeftColoredPointNoNesting(Rectangle r) {
+        if (r instanceof Rectangle(ColoredPoint ul, ColoredPoint lr)) {
+            System.out.println(ul.c());
+        }
+    }
+
+    static void printColorOfUpperLeftPointNesting(Rectangle r) {
+        if (r instanceof Rectangle(ColoredPoint(Point p, Color c),
+                                   ColoredPoint lr)) {
+            System.out.println(c);
         }
     }
 

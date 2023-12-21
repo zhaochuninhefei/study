b984f2b1b645c4aa863cc968efdb5481a732ac93
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-21 11:37:32 +0800

    提交代码

---
+++ jdk21-test/src/com/czhao/test/jdk21/TestSequencedCollections.java
@@ -0,0 +1,41 @@
+package com.czhao.test.jdk21;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+/**
+ * @author zhaochun
+ */
+public class TestSequencedCollections {
+    public static void main(String[] args) {
+        TestSequencedCollections me = new TestSequencedCollections();
+        // 打印 STRS
+        System.out.println(STR."STRS = \{ Arrays.toString(STRS) }");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(STR."----- i = \{ i } -----");
+            me.test01(i+1);
+        }
+    }
+
+    @SuppressWarnings("ManualArrayToCollectionCopy")
+    private void test01(int i) {
+        LinkedHashSet<String> setSquenced = new LinkedHashSet<>();
+        // 按顺序从 STR 中获取元素并加入 setSquenced
+        for (int j = 0; j < i; j++) {
+            setSquenced.add(STRS[j]);
+        }
+        System.out.println(setSquenced);
+
+        HashSet<String> setNotSquenced = new HashSet<>();
+        // 从 STR 中获取元素并加入 setNotSquenced
+        for (int j = 0; j < i; j++) {
+            setNotSquenced.add(STRS[j]);
+        }
+        System.out.println(setNotSquenced);
+    }
+
+    // 定义一个长度为10的String数组类型的常量
+    private static final String[] STRS = new String[]{"a3", "b3", "a2", "a1", "a0", "a10", "b1", "b2", "b0", "b10"};
+
+}

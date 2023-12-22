b9264fbc09bbd62ed5f71be953b784eb04318fd6
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-22 13:47:28 +0800

    提交代码

---
+++ jdk21-test/src/com/czhao/test/jdk21/TestSwitchPatternMatch.java
@@ -0,0 +1,12 @@
+package com.czhao.test.jdk21;
+
+/**
+ * @author zhaochun
+ */
+public class TestSwitchPatternMatch {
+    public static void main(String[] args) {
+        TestSwitchPatternMatch me = new TestSwitchPatternMatch();
+
+    }
+
+}

2f477694d98f72f4009d93d1ccefc8d3b9198621
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-26 14:29:33 +0800

    提交代码

--- jdk21-test/src/main/java/com/czhao/test/jdk21/TestVirtualThread.java
+++ jdk21-test/src/main/java/com/czhao/test/jdk21/TestVirtualThread.java
@@ -12,31 +12,43 @@ import java.util.concurrent.Future;
 /**
  * @author zhaochun
  */
+@SuppressWarnings("CallToPrintStackTrace")
 public class TestVirtualThread {
     public static void main(String[] args) {
         TestVirtualThread me = new TestVirtualThread();
         me.test01();
-//        me.testReadDbByPlatformThread();
-//        me.testReadDbByVirtualThread();
+        me.testReadDbByPlatformThread();
+        me.testReadDbByVirtualThread();
     }
 
     private void test01() {
         Thread v1 = Thread.ofVirtual().name("v1").unstarted(() -> System.out.println("run v1..."));
         v1.start();
+
+        Thread.startVirtualThread(() -> {
+            System.out.println("run v2...");
+            System.out.println("Is v2 virtual thread : " + Thread.currentThread().isVirtual());
+        });
+
+        Thread.Builder builder = Thread.ofVirtual().name("v3-", 0);
+        Runnable task = () -> {
+            var threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " run...");
+            System.out.println(" Is " + threadName + " virtual thread : " + Thread.currentThread().isVirtual());
+        };
+        Thread v3_0 = builder.start(task);
+        Thread v3_1 = builder.start(task);
+
+        Thread.getAllStackTraces().keySet().forEach(System.out::println);
+
         try {
             v1.join();
+            v3_0.join();
+            v3_1.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("v1 over...");
 
-        Thread.startVirtualThread(() -> System.out.println("run v2..."));
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         System.out.println("Main over...");
     }
 

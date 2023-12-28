9df13e6f711bb7ec67eb4a20d122e77645b6cddb
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-28 14:37:11 +0800

    提交代码

--- jdk21-test/src/main/java/com/czhao/test/jdk21/TestKEM.java
+++ jdk21-test/src/main/java/com/czhao/test/jdk21/TestKEM.java
@@ -1,6 +1,5 @@
 package com.czhao.test.jdk21;
 
-import javax.crypto.DecapsulateException;
 import javax.crypto.KEM;
 import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -16,11 +15,13 @@ public class TestKEM {
         var me = new TestKEM();
         me.test01();
         me.test02();
+        me.test03();
     }
 
     private void test01() {
         try {
             // Receiver side
+            // 接收方选择椭圆曲线X25519来生成自己的公私钥
             var kpg = KeyPairGenerator.getInstance("X25519");
             var kp = kpg.generateKeyPair();
             // 公钥
@@ -32,13 +33,17 @@ public class TestKEM {
             // Receiver公开自己的公钥 publicKeyBase64 发送给 Sender
 
             // Sender side
+            // 选择KEM算法，目前只有 DHKEM 可选:
+            // https://docs.oracle.com/en/java/javase/21/docs/specs/security/standard-names.html
             var kem1 = KEM.getInstance("DHKEM");
+            // 使用接收方公开的公钥来生成 密钥封装器
             var sender = kem1.newEncapsulator(decodePublicKeyFromBase64(publicKeyBase64));
+            // 生成密钥并封装为密钥消息
             var encapsulated = sender.encapsulate();
             // 对 encapsulated.encapsulation() 做base64编码
             var encapsulatedBase64 = Base64.getEncoder().encodeToString(encapsulated.encapsulation());
             var k1 = encapsulated.key();
-            System.out.println(Base64.getEncoder().encodeToString(k1.getEncoded()));
+            System.out.println("发送密钥1:" + Base64.getEncoder().encodeToString(k1.getEncoded()));
 
             // 模拟 Sender 将 encapsulatedBase64 发送给 Receiver
 
@@ -48,9 +53,24 @@ public class TestKEM {
             var kem2 = KEM.getInstance("DHKEM");
             var receiver = kem2.newDecapsulator(kp.getPrivate());
             var k2 = receiver.decapsulate(encapsulatedInReceiver);
-            System.out.println(Base64.getEncoder().encodeToString(k2.getEncoded()));
+            System.out.println("接收密钥1:" + Base64.getEncoder().encodeToString(k2.getEncoded()));
 
             assert Arrays.equals(k1.getEncoded(), k2.getEncoded());
+
+            // Sender side
+            // 再次生成密钥并发送
+            var encapsulated2 = sender.encapsulate();
+            var encapsulatedBase642 = Base64.getEncoder().encodeToString(encapsulated2.encapsulation());
+            var k3 = encapsulated2.key();
+            System.out.println("发送密钥2:" + Base64.getEncoder().encodeToString(encapsulated2.key().getEncoded()));
+
+            // Receiver side
+            byte[] encapsulatedInReceiver2 = Base64.getDecoder().decode(encapsulatedBase642);
+            var k4 = receiver.decapsulate(encapsulatedInReceiver2);
+            System.out.println("接收密钥2:" + Base64.getEncoder().encodeToString(k4.getEncoded()));
+
+            // 比较 k3 k4
+            assert Arrays.equals(k3.getEncoded(), k4.getEncoded());
         } catch (GeneralSecurityException e) {
             throw new RuntimeException(e);
         }
@@ -87,6 +107,18 @@ public class TestKEM {
 
         // 验证原始私钥和解码后的私钥是否相同
         System.out.println("Original Private Key equals Decoded Private Key: " + privateKey.equals(decodedPrivateKey));
+    }
+
+    private void test03() {
+        for (Provider provider : Security.getProviders()) {
+            for (Provider.Service service : provider.getServices()) {
+//                System.out.println(service.getAlgorithm());
+                var algorithm = service.getAlgorithm();
+                if (algorithm.endsWith("KEM")) {
+                    System.out.println(algorithm);
+                }
+            }
+        }
     }
 
 

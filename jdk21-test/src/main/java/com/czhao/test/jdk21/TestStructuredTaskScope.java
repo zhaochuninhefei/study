192583b09eb661a72dea760c1f6149ed0baedc04
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-12-28 10:25:50 +0800

    提交代码

--- jdk21-test/src/main/java/com/czhao/test/jdk21/TestStructuredTaskScope.java
+++ jdk21-test/src/main/java/com/czhao/test/jdk21/TestStructuredTaskScope.java
@@ -28,12 +28,12 @@ public class TestStructuredTaskScope {
     private Response handle() throws ExecutionException, InterruptedException {
         // 定义一个 结构化任务的作用域 StructuredTaskScope, 并指定关闭策略为 ShutdownOnFailure
         try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-            // 在作用域上创建一个分支, 传入子任务定义(lambda)
+            // 在作用域上创建一个分支, 传入子任务定义(lambda)，即分派一个子任务
             Supplier<String> user = scope.fork(this::findUser);
-            // 在作用域上创建另一个分支, 传入子任务定义(lambda)
+            // 在作用域上创建另一个分支, 传入子任务定义(lambda)，即分派一另个子任务
             Supplier<Integer> order = scope.fork(this::fetchOrder);
 
-            // join 加入两个子任务
+            // join 将已经分派的子任务加入作用域
             scope.join()
                     // 传播错误
                     .throwIfFailed();
@@ -44,10 +44,16 @@ public class TestStructuredTaskScope {
     }
 
     private String findUser() {
+        // 打印当前线程信息
+        System.out.println("findUser current thread: " + Thread.currentThread().threadId() + " isVirtual: " + Thread.currentThread().isVirtual());
+        // 假设这里从表1读取用户名
         return "tester";
     }
 
     private Integer fetchOrder() {
+        // 打印当前线程信息
+        System.out.println("fetchOrder current thread: " + Thread.currentThread().threadId() + " isVirtual: " + Thread.currentThread().isVirtual());
+        // 假设这里从表2读取订单ID
         return 1;
     }
 }

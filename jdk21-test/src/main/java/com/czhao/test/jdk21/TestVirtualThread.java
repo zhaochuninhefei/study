53ec662de3dad2671a5043c5f66acf14f39c7733
zhaochuninhefei <zhaochun.hefei@qq.com>
2024-01-11 14:22:55 +0800

    提交代码

--- jdk21-test/src/main/java/com/czhao/test/jdk21/TestVirtualThread.java
+++ jdk21-test/src/main/java/com/czhao/test/jdk21/TestVirtualThread.java
@@ -13,13 +13,16 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author zhaochun
  */
-@SuppressWarnings("CallToPrintStackTrace")
+@SuppressWarnings({"CallToPrintStackTrace", "StringTemplateMigration"})
 public class TestVirtualThread {
     public static void main(String[] args) {
         TestVirtualThread me = new TestVirtualThread();
         me.test01();
         me.test02();
         me.test03();
+        me.test04();
+        me.test05();
+        me.test06();
         me.testReadDbByPlatformThread();
         me.testReadDbByVirtualThread();
     }
@@ -84,18 +87,81 @@ public class TestVirtualThread {
     }
 
     private void test03() {
+        // 不要同时使用 try-with-resource 和 Executors.newSingleThreadScheduledExecutor()
+        // 比如下面的例子，如果不在 scheduleWithFixedDelay 之后，try-with-resource 花括弧`}`之前 sleep一秒的话，
+        // 那么在计划器延迟启动任务之前，就会执行 try-with-resource 对 executor 的 close，该方法会尝试关闭 executor，
+        // 而无论关闭的时间点是在计划器首次执行任务之前，还是在两次执行任务的中间，executor都会被关闭而导致计划任务不再执行。
         try (var executor = Executors.newSingleThreadScheduledExecutor(Thread.ofVirtual().factory())) {
             executor.scheduleWithFixedDelay(() -> {
-                System.out.println(Thread.currentThread().threadId() + " isVirtual:" + Thread.currentThread().isVirtual());
+                System.out.println(STR."\{Thread.currentThread().threadId()} isVirtual:\{Thread.currentThread().isVirtual()}");
                 System.out.println("test03 executor.scheduleWithFixedDelay...");
-            }, 3, 3, TimeUnit.SECONDS);
-            Thread.sleep(10000);
-            executor.shutdown();
-            var waitResult = executor.awaitTermination(60, TimeUnit.SECONDS);
-            System.out.println("waitResult:" + waitResult);
+            }, 1, 1, TimeUnit.SECONDS);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        System.out.println("test03 继续...");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("test03 结束...");
+    }
+
+    private void test04() {
+        try (var executor = Executors.newSingleThreadScheduledExecutor()) {
+            executor.scheduleWithFixedDelay(() -> {
+                System.out.println(STR."\{Thread.currentThread().threadId()} isVirtual:\{Thread.currentThread().isVirtual()}");
+                System.out.println("test04 executor.scheduleWithFixedDelay...");
+            }, 1, 1, TimeUnit.SECONDS);
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("test04 继续...");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("test04 结束...");
+    }
+
+    private void test05() {
+        // schedule的话，try-with-resource 会在花括弧结束后等待计划任务执行
+        try (var executor = Executors.newSingleThreadScheduledExecutor()) {
+            executor.schedule(() -> {
+                System.out.println(STR."\{Thread.currentThread().threadId()} isVirtual:\{Thread.currentThread().isVirtual()}");
+                System.out.println("test05 executor.scheduleWithFixedDelay...");
+            }, 3, TimeUnit.SECONDS);
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("test05 继续...");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("test05 结束...");
+    }
+
+    private void test06() {
+        @SuppressWarnings("resource")
+        var executor = Executors.newSingleThreadScheduledExecutor(Thread.ofVirtual().factory());
+        executor.scheduleWithFixedDelay(() -> {
+            System.out.println(STR."\{Thread.currentThread().threadId()} isVirtual:\{Thread.currentThread().isVirtual()}");
+            System.out.println("test06 executor.scheduleWithFixedDelay...");
+        }, 1, 1, TimeUnit.SECONDS);
+        System.out.println("test06 继续...");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("test06 结束...");
     }
 
     private static final int CNT_THREADS = 300;
